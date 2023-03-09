9dc70818b4e8edd0ad7329b9c0f4478b8adab89a
Toto <santagadatn@gmail.com>
2023-03-09 13:58:21 -0300

    add ssh function

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "\e[35m\nedit bashrc\e[0m"
+echo -e "\e[35m\nEdit bashrc\e[0m"
 sudo sed -i -e '/# Tunning section/,$d' ~/.bashrc
 sudo cat bashTunning.bashrc >> ~/.bashrc
 
-echo -e "\e[35m\nsource ~/.bashrc\n\e[0m"
+echo -e "\e[35m\n. .bashrc\n\e[0m"
 source ~/.bashrc
